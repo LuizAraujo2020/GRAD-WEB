ab9f059a349a2cf709d7a48be2c4c5bcc5994519
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2024-03-14 10:20:58 -0300

    git feat: answer for the first activity

---
+++ atividade01/variaveisOperadores.js
@@ -0,0 +1,36 @@
+
+// Operators
+const operators = ['soma', 'subtração', 'multiplicação', 'divisão']
+
+// Values
+let number1 = 5
+let number2 = 7
+
+
+function calculate(operator, num1, num2)  {
+    switch(operator) {
+        case 'soma':
+          return num1 + num2
+          break;
+        case 'subtração':
+        return num1 - num2
+        break;
+        case 'multiplicação':
+            return num1 * num2
+            break;
+        case 'divisão':
+            return num1 + num2
+            break;
+        default:
+          return 0
+      }
+}
+
+function printResult(operator, num1, num2) {
+    console.log(`Resultado da ${operator} entre ${num1} e ${num2}: ${calculate(operator, num1, num2)}`)
+}
+
+
+for (const op in operators) {
+    printResult(operators[op], number1, number2)
+}
