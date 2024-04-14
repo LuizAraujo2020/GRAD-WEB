6c6822575da911155a8244003d9dfa8c57551e65
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2024-04-14 10:13:39 -0300

    feat: estoque factory

--- modulos/factories/estoque_factory.js
+++ modulos/factories/estoque_factory.js
@@ -1,3 +1,7 @@
+
+const path = require('path');
+// path.join(__dirname, '/index.html')
+
 function criarListaDeProdutos(produtos) {
     return `
     <!DOCTYPE html>
@@ -8,6 +12,19 @@ function criarListaDeProdutos(produtos) {
         <title>Listar Produtos</title>
     </head>
     <body>
+        <h1>Lista de Produtos</h1>
+        
+        <form action="/adicionar" method="get">
+
+            <label for="nome">NOME: </label>
+            <input type="text" name="nome" id="nome">
+
+            <label for="qtd">QTD: </label>
+            <input type="text" name="qtd" id="qtd">
+            
+            <button type="submit">➕</button>
+        </form>
+        <br>
         <table>
             <tr>
                 <th>ID</th>
@@ -41,6 +58,18 @@ function criarLinha(produto) {
             <td>${produto.id}</td>
             <td>${produto.nome}</td>
             <td>${produto.qtd}</td>
+            
+            <td>
+                <form action="/remover/${produto.id}" method="get">
+                    <button type="submit">❌</button>
+                </form>
+            </td>
+
+            <td>
+                <form action="/detalhes/${produto.id}/" method="get">
+                    <button type="submit">🔎</button>
+                </form>
+            </td>
         </tr>
     `;
 }
@@ -53,6 +82,38 @@ function criarLinhaVazia() {
     `;
 }
 
+/// DETAILS
+function criarDetalhesDeProduto(produto) {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Listar Produtos</title>
+        
+    </style>
+    </head>
+    <body>
+        <form action="/editar">
+        <label for="">ID: ${produto.id}</label><br><br>
+        <input type="text" name="id" id="id" value="${produto.id}" hidden>
+
+        <label for="">Nome</label>
+        <input type="text" name="nome" id="nome" value="${produto.nome}">
+
+        <br><br>
+
+        <label for="">QTD</label>
+        <input type="text" name="qtd" id="qtd" value="${produto.qtd}">
+
+        <button type="submit">SALVAR</button>
+    </form>
+    </body>
+    </html>
+    `;
+}
 module.exports = {
-    criarListaDeProdutos
+    criarListaDeProdutos,
+    criarDetalhesDeProduto
 }
