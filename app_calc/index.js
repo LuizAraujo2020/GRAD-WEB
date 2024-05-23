535698e5452ffea92cb9eff1683d7d82b5f38e4c
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2024-05-23 10:39:06 -0300

    feat: router and controller

--- app_calc/index.js
+++ app_calc/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const mustacheExpress = require('mustache-express')
 
+// const controller = require('./src/controller/calculadoraController') 
+const router = require('./src/routes/calculadoraRoutes') 
+
 const app = express()
 
 app.engine('html', mustacheExpress())
@@ -10,25 +13,7 @@ app.set('views', __dirname + "/src/views")
 app.use(express.urlencoded({ extended: true }))
 
 // ROTAS
-app.get('/', (req, res) => {
-    let erro_valor = req.query.erro_valor
-
-    res.render('index', { erro_valor })
-})
-
-app.post('/resultado', (req, res) => {
-    let v1 = Number(req.body.v1)
-    let v2 = Number(req.body.v2)
-    let op = req.body.op
-
-    if(isNaN(v1) || isNaN(v2)) {
-        res.redirect('/?erro_valor=true')
-    } else {
-        v3 = v1 + v2
-        res.render('resultado', { v1, v2, op , v3 })
-    }
-
-})
+app.use('/', router)
 
 const PORT = 5500
 app.listen(PORT, function () {
---
+++ app_calc/src/controller/calculadoraController.js
@@ -0,0 +1,25 @@
+function index(req, res) {
+
+    let erro_valor = req.query.erro_valor
+
+    res.render('index', { erro_valor })
+}
+
+function resultado(req, res) {
+
+    let v1 = Number(req.body.v1)
+    let v2 = Number(req.body.v2)
+    let op = req.body.op
+
+    if(isNaN(v1) || isNaN(v2)) {
+        res.redirect('/?erro_valor=true')
+    } else {
+        v3 = v1 + v2
+        res.render('resultado', { v1, v2, op , v3 })
+    }
+}
+
+module.exports = {
+    index,
+    resultado
+}
